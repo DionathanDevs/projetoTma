f955e3e4e693b119ee68e26e525457cc732b06c9
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-02-13 12:21:40 -0300

    Alterações de formulário e Design.
    Form alterado e design.

--- tma/page/tma_new.php
+++ tma/page/tma_new.php
@@ -9,65 +9,110 @@ $dataTma = $tmaClass->getAll();
 
 <h2 class="titulo-pagina"><span><i class="bi bi-broadcast-pin"></i></span>TMA > DataLoggers > Adicionando</h2>
 
+
 <section class="form-box">
 
-<form class="form-add">
-<input type="hidden" id="field0" name="id_tma">
-                <div class="box-add-form">
-                    <label for="field1">TMA</label>
-                    <input type="text" id="field1" name="name">
-                </div>
-                <div class="box-add-form">
-                    <label for="field2">Device Id</label>
-                    <input type="text" id="field2" name="datalog_deviceid" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field3">Número DataLogger</label>
-                    <input type="text" id="field3" name="datalog_number" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field4">Modelo</label>
-                    <input type="text" id="field4" name="datalog_model" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field5">Software</label>
-                    <input type="text" id="field5" name="datalog_software" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field6">Login</label>
-                    <input type="text" id="field6" name="datalog_login" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field7">Password</label>
-                    <input type="text" id="field7" name="datalog_password">
+
+<div class="container-form">
+
+<form class="form-datalog">
+
+        <div class="form-first">
+
+            <div class="datalog-info">
+
+                <h3 class="header-title">Cadastro DataLogger</h3>
+
+
+                <div class="fields">
+
+                    <div class="input-field">
+
+                        <label for="">Tma</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Device Id</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Número DataLogger</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Modelo</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Software</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Login</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Password</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Ip</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Máscara</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Gateway</label>
+                        <input type="text">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Rack Painel</label>
+                        <input type="text" id="field12">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Rack SW</label>
+                        <input type="text" id="field13">
+
+                    </div>
+                    <div class="input-field">
+
+                        <label for="">Porta SW</label>
+                        <input type="text" id="field14">
+
+                    </div> 
+                    
                 </div>
 
-                <div class="box-add-form">
-                    <label for="field9">Gateway</label>
-                    <input type="text" id="field9" name="datalog_ip" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field10">Máscara</label>
-                    <input type="text" id="field10" name="datalog_mask" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field11">Gateway</label>
-                    <input type="text" id="field11" name="datalog_gateway" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field12">Rack Painel</label>
-                    <input type="text" id="field12" name="rack_panel" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field13">Rack SW</label>
-                    <input type="text" id="field13" name="rack_sw" >
-                </div>
-                <div class="box-add-form">
-                    <label for="field14">Porta SW</label>
-                    <input type="text" id="field14" name="rack_sw_port">
-                </div>
-                <div class="box-btn-salvar">
-                    <button class="btn-salvar" type="submit">Salvar</button>
-                </div>
+               <button class="saveBtn">
+                        <span class="btnText">Salvar</span>
+                </button>
+
+            </div>
+        </div>
+</div>
 </form>
 </section>
